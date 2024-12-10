c1982d9fbafc3c1e565dfaa0d5717ccb393655e1
ANITA\anita <balasanyananita@gmail.com>
2024-12-10 19:47:51 +0400

    fix: better visuality

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -43,7 +43,7 @@ function RegisterForm({ onClose, setIsLogin }) {
                     <div className="modal-content">
                         <div className="modal-header">
                             <h5 className="modal-title">Register</h5>
-                            <button type="button" className="close" onClick={onClose}>
+                            <button type="button" className="btn-close" onClick={onClose}>
                                 <span aria-hidden="true">&times;</span>
                             </button>
                         </div>
